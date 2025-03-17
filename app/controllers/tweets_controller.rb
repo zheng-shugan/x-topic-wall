7b9fed1be6203f6b97b72271aad51d7ac194a655
zhengshugan <zhengshugan@foxmail.com>
2025-03-17 23:19:50 +0800

    feat: 首页和详情页界面优化

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,9 +1,11 @@
 class TweetsController < ApplicationController
   def index
     @tweets = Tweet.all()
+    p @tweets
   end
 
   def show
+    p params[:id]
     @tweet = Tweet.find_by!(tweet_id: params[:id])
   end
 end
