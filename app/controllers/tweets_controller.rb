515bd48cbc5278205ecb42368f13107a49376077
zhengshugan <zhengshugan@foxmail.com>
2025-03-18 20:34:39 +0800

    feat: 404 页面和样式修改

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,11 +1,13 @@
 class TweetsController < ApplicationController
   def index
-    @tweets = Tweet.all()
+    @tweets = Tweet.all
     p @tweets
   end
 
   def show
     p params[:id]
     @tweet = Tweet.find_by!(tweet_id: params[:id])
+  rescue ActiveRecord::RecordNotFound
+    render file: Rails.root.join("public/404.html"), status: :not_found, layout: false
   end
 end
